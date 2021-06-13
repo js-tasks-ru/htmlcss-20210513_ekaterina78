4a41043686704d280a591f88c89a5f3b681678b7
Ekaterina Driga <ekaterina.driga@t-systems.com>
2021-06-13 12:16:08 +0300

    add solution for 09-lection7-burger-menu

--- 10-lection8/01-theme-changer/theme-changer.js
+++ 10-lection8/01-theme-changer/theme-changer.js
@@ -1,10 +1,12 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const toggleSwitches = document.querySelectorAll('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
+        for (const toggleSwitch of toggleSwitches) {
+            toggleSwitch.checked = true;
+        }
     }
 }
 
@@ -12,11 +14,19 @@ function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
+        for (const toggleSwitch of toggleSwitches) {
+            toggleSwitch.checked = true;
+        }
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
+        for (const toggleSwitch of toggleSwitches) {
+            toggleSwitch.checked = false;
+        }
     }
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+for (const toggleSwitch of toggleSwitches) {
+    toggleSwitch.addEventListener('change', switchTheme, false);
+}
